0b2073837abb594ebe6589d11c952810ea5523f6
Lior Vainer <lior.vainer@agamimp.net>
2023-10-06 19:16:33 +0300

    Initial Commit with home and about pages with links between them

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/about">About Me</Link>
+      <Link href="/">Home</Link>
     </main>
   );
 }
