d18f2baa3f14a5fe81d7262adac1c9256cce9384
Lior Vainer <lior.vainer@agamimp.net>
2023-11-03 21:00:10 +0200

    feat(HomePage): added some struct and design to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import Link from "next/link";
+import "./page.css";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="container background-animate">
+      <header className="flex-col items-start justify-center gap-3 ">
+        <h1 className="text-6xl font-bold text-white">Lior Vainer</h1>
+        <h4 className="text-4xl font-normal text-white">
+          Full Stack Developer
+        </h4>
+      </header>
       <Link href="/about">About Me</Link>
+
+      <div className="p-24 bg-white rounded-sm">
+        <h2 className="text-sm font-bold text-black">Description</h2>
+      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      backgroundColor: {
+        purple: {
+          100: "#e3d2f0",
+          200: "#c7a5e1",
+          300: "#aa77d1",
+          400: "#8e4ac2",
+          500: "#721db3",
+          600: "#5b178f",
+          700: "#44116b",
+          800: "#2e0c48",
+          900: "#170624",
+        },
       },
     },
   },
+  darkMode: "class",
   plugins: [],
-}
-export default config
+};
+export default config;
