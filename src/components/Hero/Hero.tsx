faf234fe8dcb301cf596eb66c1971399acc83ebe
Lior Vainer <lior.vainer@agamimp.net>
2023-11-13 11:12:44 +0200

    feat(Hero): added scroll indicator and animation of word by word for text

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,28 +1,58 @@
+import { motion } from "framer-motion";
+import { PROFILE } from "../../constants/profile.const";
+import classes from "./hero.module.scss";
+
 export interface HeroProps {}
 
 export const Hero = (props: HeroProps) => {
+  const first = PROFILE.FIRST.split(" ");
+  const second = PROFILE.SECOND.split(" ");
+
   return (
     <div className="h-screen p-52 flex flex-col justify-center items-start gap-16">
       <header className="flex-col items-start justify-center gap-4">
         <h1 className="text-8xl font-bold drop-shadow-lg text-white">
           Lior Vainer
         </h1>
-        <h4 className="text-6xl font-normal text-white">
+        <h4 className="text-6xl font-normal text-zinc-300">
           Full Stack Developer
         </h4>
       </header>
 
       <article className="flex flex-col gap-16">
-        <p className="text-5xl font-medium text-white">
-          I'm an experienced software engineer, specializing in full-stack
-          development. I'm passionate about UI/UX design and stay up-to-date
-          with the latest tech trends.
+        <p className="text-5xl font-medium">
+          {first.map((el, i) => (
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{
+                duration: 0.25,
+                delay: i / 20,
+              }}
+              key={i}
+            >
+              {el}{" "}
+            </motion.span>
+          ))}
         </p>
         <p className="text-5xl font-medium text-white">
-          I'm driven by a strong work ethic to deliver top-notch results. I also
-          have hands-on experience with cloud platforms and CI/CD for
-          streamlined deployment.
+          {second.map((el, i) => (
+            <motion.span
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{
+                duration: 0.25,
+                delay: 3 + i / 20,
+              }}
+              key={i}
+            >
+              {el}{" "}
+            </motion.span>
+          ))}
         </p>
+        <div className="flex items-center w-full justify-center">
+          <div className={classes.mouse}></div>
+        </div>
       </article>
     </div>
   );
